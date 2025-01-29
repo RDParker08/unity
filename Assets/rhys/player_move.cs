2e1282867b8dfa5df9c407ae2219032f87480572
RDParker08 <rdparker8002@gmail.com>
2025-01-29 13:36:17 +0000

    tilemap and charcter mo9vement and animation

--- Assets/rhys/player_move.cs
+++ Assets/rhys/player_move.cs
@@ -8,9 +8,13 @@ public class player_move : MonoBehaviour
     public float speed;
     private Rigidbody2D myRigidbody;
     private Vector3 change;
+    private Animator animator;
     
+
+
     private void Start()
     {
+        animator = GetComponent<Animator>();
         myRigidbody = GetComponent<Rigidbody2D>();
     }
 
@@ -20,11 +24,25 @@ public class player_move : MonoBehaviour
         change = Vector2.zero;
         change.x = Input.GetAxisRaw("Horizontal");
         change.y = Input.GetAxisRaw("Vertical");
-            if(change != Vector3.zero)
-        {
-            MoveCharacter();
+        updateanimationandmove();
         }
-    }
+     
+    void updateanimationandmove()
+        {
+            if (change != Vector3.zero)
+            {
+                MoveCharacter();
+                animator.SetFloat("moveX", change.x);
+                animator.SetFloat("moveY", change.y);
+                animator.SetBool("moving", true);
+            }
+            else
+            {
+                animator.SetBool("moving", false);
+            }
+        }
+
+
 
     void MoveCharacter()
     {
