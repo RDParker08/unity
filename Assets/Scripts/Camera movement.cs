8dfef9d91c701d750ae98a8a27f9dceb3411c87a
RDParker08 <rdparker8002@gmail.com>
2025-02-24 15:45:47 +0000

    l

--- Assets/Scripts/Camera movement.cs
+++ Assets/Scripts/Camera movement.cs
@@ -24,12 +24,12 @@ public class Cameramovement : MonoBehaviour
             Vector3 targetPosition = new Vector3(target.position.x, 
                                                  target.position.y, 
                                                  transform.position.z);
-            targetPosition.x = Mathf.Clamp(targetPosition.x,
-                                           minPosition.x,
-                                           maxPosition.x);
-            targetPosition.y = Mathf.Clamp(targetPosition.y,
-                                           minPosition.y,
-                                           maxPosition.y);
+            //targetPosition.x = Mathf.Clamp(targetPosition.x,
+            //                               minPosition.x,
+            //                               maxPosition.x);
+            //targetPosition.y = Mathf.Clamp(targetPosition.y,
+            //                               minPosition.y,
+            //                               maxPosition.y);
 
             transform.position = Vector3.Lerp(transform.position, 
                                               targetPosition, smoothing);
